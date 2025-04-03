29dd7e5e2c82a93b495e8295901ea755eb1e5fe1
Dip Patel <pateldeep2332@gmail.com>
2025-04-03 13:30:58 +0530

    Added More in merge sort

--- Sorting-Stick/include/Gameplay/Collection/StickCollectionController.h
+++ Sorting-Stick/include/Gameplay/Collection/StickCollectionController.h
@@ -47,6 +47,8 @@ namespace Gameplay
             void inPlaceMerge(int left, int mid, int right);
             void inPlaceMergeSort(int left, int right);
             void processMergeSort();
+            void mergeSort(int left, int right);
+            void merge(int left, int mid, int right);
             void processSortThreadState();
 
             bool isCollectionSorted();
